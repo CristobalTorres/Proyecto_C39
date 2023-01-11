ea41807653bb7f6d30b19b42609e4b70a7c4f8f1
CristobalTorres <cristobal.torres6771@gmail.com>
2023-01-11 10:56:59 -0500

    archivos

---
+++ js/Quiz.js
@@ -0,0 +1,78 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  up_date(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    
+    question.hide(); 
+
+    background("Yellow");
+    fill(0);
+    textSize(30);
+    text("Resultados del quiz",340, 50);
+    text("----------------------------",330, 65);
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
+      debugger;
+      var display_Answers = 270;
+      fill("Blue");
+      textSize(20);
+      text("*NOTA: ¡Los participantes que respondieron correctamente están resaltados en color verde!",20,230);
+
+      for(var plr in allContestants){
+        debugger;
+        var correctAns="2";
+        
+        //reto1
+        if(allContestants !== correctAns){
+          fill("red");
+           
+        }
+        if(allContestants === correctAns){
+          fill("green");
+          
+        }
+        textSize(20);
+        display_Answers+=30;
+        text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers) 
+        
+        
+        
+       
+        
+        
+         
+        
+      }
+    }
+  }
+}
